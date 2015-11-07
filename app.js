3740c0f2fbe1f06476ed998f7a5fb8d6bbee4003
James Wang <jw@slickage.com>
2015-11-06 19:07:20 -1000

    SPA endpoint loads index.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 app.set('port', process.env.PORT || 8080);
 app.use(express.static(path.join(__dirname, '/public')));
 app.get('*', function (req, res) {
-	res.sendFile('index.html');
+	res.sendFile('index.html', {root: __dirname + '/public'});
 });
 
 var port = app.get('port');
