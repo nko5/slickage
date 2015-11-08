234ee2cfb4e7e5f49806f8ad811f076c53578d76
unenglishable <unenglishable@gmail.com>
2015-11-08 11:41:58 -1000

    fix focus problem (oops, tagged wrong commit)
    actually fixes #1

--- public/js/index.js
+++ public/js/index.js
@@ -87,6 +87,6 @@ setInterval(function() {
   }
   if (imgq.length > 0) {
     shownImage = m.prop(imgq.shift());
-    m.module(document.getElementById('imageView'), imageView);
+    m.render(document.getElementById('imageView'), imageView);
   }
 }, 4000 /* you never need to watch more than 4 seconds of any gif, ever. */);
